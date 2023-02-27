ff8dba80f86b03d0ab718f36c1dd06b7e6bf8c5a
Kuruppu K.A.G.S.R. it21165252 <it21165252@my.sliit.lk>
2023-02-27 10:14:27 +0530

    wip

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    lateinit var edtNumber1:EditText
+    lateinit var edtNumber2:EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        edtNumber1 = findViewById(R.id.edit_1)
+        edtNumber2 = findViewById(R.id.edit_2)
     }
+}
+
+fun buttonClick(v:View){
+    var ans = 0.0
+    val calculator = Calculator(
+        edtNumber1.text.toString().toDouble(),
+        edtNumber2.text.toString().toDouble())
+    when(v.id){
+        R.id.btnPlus -> ans = calculator.add()
+        R.id.btnMinus -> ans = calculator.subtract()
+        R.id.btnMultiply -> ans = calculator.multiply()
+        R.id.btnDivide -> ans = calculator.divide()
+    }
+    println(ans)//this will print the output on the terminal
 }
